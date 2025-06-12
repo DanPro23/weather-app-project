bf5c97b2165b89ae9a42366eb31da99bb96a7a9f
Данило <proydisvetdanila@gmail.com>
2025-06-12 20:51:41 +0300

    new info into requirements

--- weather_app/weather_app/settings.py
+++ weather_app/weather_app/settings.py
@@ -9,18 +9,18 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
 from pathlib import Path
+from dotenv import loadenv
+loadenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-sn93kkz@wr_0z*8ve0!1@a^z(%+41mym*sp04-353t89b345qm'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
